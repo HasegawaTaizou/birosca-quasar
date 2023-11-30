c39c04009e355e03b218d5b8346490e6c6d8d666
HasegawaTaizou <caiocoghi@gmail.com>
2023-11-30 16:51:46 -0300

    alo

---
+++ src/pages/MainLayout.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-layout>
+    <router-view />
+  </q-layout>
+</template>
+
+<script>
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  name: "MainLayout",
+  components: {},
+  setup() {},
+});
+</script>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,15 +1,21 @@
+import ErrorNotFoundVue from "pages/ErrorNotFound.vue";
+
 const routes = [
   {
     path: "/",
-    component: () => import("layouts/MainLayout.vue"),
-    children: [{ path: "", component: () => import("pages/IndexPage.vue") }],
+    component: () => import("src/pages/MainLayout.vue"),
+  },
+  {
+    path: "/",
+    component: () => import("src/pages/MainLayout.vue"),
   },
 
   // Always leave this as last one,
   // but you can also remove it
   {
     path: "/:catchAll(.*)*",
-    component: () => import("pages/ErrorNotFound.vue"),
+    name: "ErrorNotFound",
+    component: ErrorNotFoundVue,
   },
 ];
 
