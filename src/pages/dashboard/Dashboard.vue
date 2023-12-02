379cd337de5ba44014fb758bfd96827e729eeda4
HasegawaTaizou <caiocoghi@gmail.com>
2023-12-02 14:20:09 -0300

    alo

--- src/pages/dashboard/Dashboard.vue
+++ src/pages/dashboard/Dashboard.vue
@@ -1,35 +1,51 @@
 <template>
   <q-layout view="lhh Lpr lff">
     <q-drawer
-      class="sidebar"
       v-model="drawer"
       show-if-above
       :mini="miniState"
       :breakpoint="500"
       elevated
+      :class="$style.sidebar"
     >
-      <q-scroll-area class="fit" :horizontal-thumb-style="{ opacity: 0 }">
-        <q-list tag="ul">
-          <q-item tag="li" to="/dashboard/foods">
-            <q-item-section avatar>
-              <q-icon name="fas fa-ambulance" />
-            </q-item-section>
-            <q-item-section> Inbox </q-item-section>
-          </q-item>
-          <q-item tag="li" to="/dashboard/orders">
-            <q-item-section avatar>
-              <q-icon name="fas fa-ambulance" />
-            </q-item-section>
-            <q-item-section> Inbox </q-item-section>
-          </q-item>
-          <q-item tag="li" to="/products">
-            <q-item-section avatar>
-              <q-icon name="fas fa-ambulance" />
-            </q-item-section>
-            <q-item-section> Inbox </q-item-section>
-          </q-item>
-        </q-list>
-      </q-scroll-area>
+      <span :class="$style.title">Birosca do Açaí</span>
+      <q-list tag="ul" class="navigation">
+        <q-item
+          tag="li"
+          :class="$style.navigation__item"
+          to="/dashboard/foods"
+          :active-class="$style.active"
+        >
+          <q-item-section class="item__icon-container" avatar>
+            <q-icon name="fas fa-ambulance" />
+          </q-item-section>
+          <q-item-section :class="$style.item__name"> Comidas </q-item-section>
+        </q-item>
+        <q-item tag="li" to="/dashboard/orders" :active-class="$style.active">
+          <q-item-section avatar>
+            <q-icon name="fas fa-ambulance" />
+          </q-item-section>
+          <q-item-section> Pedidos </q-item-section>
+        </q-item>
+        <q-item tag="li" to="/dashboard/history">
+          <q-item-section avatar>
+            <q-icon name="fas fa-ambulance" />
+          </q-item-section>
+          <q-item-section> Histórico </q-item-section>
+        </q-item>
+        <q-item tag="li" to="/dashboard/settings">
+          <q-item-section avatar>
+            <q-icon name="fas fa-ambulance" />
+          </q-item-section>
+          <q-item-section> Configurações </q-item-section>
+        </q-item>
+      </q-list>
+      <q-item class="log-out" clickable>
+        <q-item-section avatar>
+          <q-icon name="fas fa-ambulance" />
+        </q-item-section>
+        <q-item-section> Sair </q-item-section>
+      </q-item>
     </q-drawer>
     <q-page-container>
       <router-view></router-view>
@@ -52,6 +68,10 @@ export default {
 };
 </script>
 
-<style scoped>
+<style>
 @import url("../../css/dashboard/dashboardStyle.css");
+</style>
+
+<style module>
+@import url("../../css/dashboard/sidebarStyle.css");
 </style>
--- src/pages/dashboard/Foods.vue
+++ src/pages/dashboard/Foods.vue
@@ -1,11 +1,68 @@
 <template>
   <q-page>
-    <div>foods</div>
+    <h1>Comidas</h1>
+    <q-list class="foods__options" tag="ul">
+      <q-item
+        :class="{ active: activeOption === 'option1' }"
+        class="option-container"
+        tag="li"
+        clickable
+        @click="activeOption = 'option1'"
+      >
+        <img
+          class="option__image"
+          :src="
+            activeOption === 'option1'
+              ? '../../assets/img/acai-black-icon.png'
+              : '../../assets/img/acai-white-icon.png'
+          "
+          alt="Acai Icon"
+        />
+        <span class="option__text">Açaís</span>
+      </q-item>
+      <q-item
+        :class="{ active: activeOption === 'option2' }"
+        class="option-container"
+        tag="li"
+        clickable
+        @click="activeOption = 'option2'"
+      >
+        <img
+          class="option__image"
+          :src="'../../assets/img/acai-black-icon.png'"
+          alt="Acai Icon"
+        />
+        <span class="option__text">Açaís</span>
+      </q-item>
+    </q-list>
+    <q-page-sticky position="bottom-right" :offset="[64, 38]">
+      <q-btn
+        type="button"
+        @click="addFood"
+        fab
+        icon="fa-solid fa-plus"
+        class="add-food__button"
+      />
+    </q-page-sticky>
   </q-page>
 </template>
 
 <script>
 export default {
   name: "Foods",
+  data() {
+    return {
+      activeOption: "",
+    };
+  },
+  methods: {
+    addFood() {
+      console.log("click");
+    },
+  },
 };
 </script>
+
+<style scoped>
+@import url("../../css/dashboard/foods/foodsStyle.css");
+</style>
